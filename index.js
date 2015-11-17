f7b2e7e0e69d2f12bcd5f5a39e358191160f7e3a
Konstantin Azizov <azizovkostya97@gmail.com>
2015-11-17 11:44:52 +0200

    Implement submit issue functional
    - Using github module
    - Update dbController usage
    - Add server-side validation for payload params

--- index.js
+++ index.js
@@ -1,28 +1,84 @@
 var Hapi = require('hapi');
 var Good = require('good');
-var Github = require('./lib/github');
+var Joi = require('joi');
+var GitHubApi = require("github");
+var DBCtrl = require('./lib/dbController');
+
+const DEBUG = process.env.DEBUG;
+
+var github = new GitHubApi({
+    version: "3.0.0",
+    debug: DEBUG,
+    protocol: "https",
+    timeout: 5000,
+    headers: {
+        "user-agent": "SubmitIssueBot"
+    }
+});
+
+github.authenticate({
+    type: "basic",
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+});
 
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 3000 });
 
 server.route({
     method: 'POST',
-    path: '/api/register/{code}',
+    path: '/api/register',
     handler: function(request, reply) {
-        Github.getToken(request.params.code, function(err, userID) {
-            if(err) console.error(err);
-            console.log('userID:', userID);
-            reply('http://SubmitAnIssue.github.io/finish?token=' + userID);
-        });
+        var repoID = DBCtrl.addRepo(request.payload.repo)
+        reply(repoID);
+    },
+    config: {
+        validate: {
+            payload: {
+                repo: Joi.string().min(3).required()
+            }
+        }
     }
 });
 
 server.route({
-    method: 'GET',
+    method: 'POST',
     path: '/api/submit',
     handler: function(request, reply) {
-        console.log('Got request with params:', request.params);
-        Github.submitIssue(request.params.userID, request.params.data)
+        var data = request.payload.data;
+        var msg = {
+            'title': data.title,
+            'body': data.body            
+        }
+        
+        var repo = DBCtrl.findRepo(request.payload.repoID, function(err, data) {
+            if(err) console.error(err);
+            if(DEBUG) console.log(data);
+            
+            msg.user = data.repository.split('/')[0];
+            msg.repo = data.repository.split('/')[1];
+            
+            github.issues.create(msg, function(err, data) {
+                if(err) {
+                    console.log(err);
+                    return reply(err.message, null)
+                                .code(404);
+                } else {
+                    return reply(data);
+                }
+            });
+        });
+    },
+    config: {
+        validate: {
+            payload: {
+                repoID: Joi.string().min(36).max(36).required(),
+                data: Joi.object().keys({
+                    title: Joi.string().required(),
+                    body: Joi.string()
+                }).required()
+            }
+        }
     }
 });
 
