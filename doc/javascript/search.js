f84246ed36348ac9dc42d0da5cec812ee0bb70f1
Konstantin Azizov <azizovkostya97@gmail.com>
2015-11-18 11:03:22 +0200

    Update documentation
    - Replace codo with yuidoc
    - Rewritten documentation for js version
    - Add new generated documentation

--- doc/javascript/search.js
+++
@@ -1,1 +0,0 @@
-window.searchData = [{"t":"DBController","p":"class/DBController.html"},{"t":"src/dbController.coffee","p":"file/src/dbController.coffee.html"},{"t":"README.md","p":"extra/README.md.html"},{"t":"LICENSE","p":"extra/LICENSE.html"},{"t":"DBController.addRepo","p":"class/DBController.html#addRepo-static"},{"t":"DBController.findRepo","p":"class/DBController.html#findRepo-static"}]
--- src/dbController.js
+++ src/dbController.js
@@ -13,13 +13,35 @@ db.bind('user').bind({
     }
 });
 
+/**
+ * Controller for interacting with MongoDB
+ *
+ * @class DBController
+ */
 var DBController = (function() {
+    /**
+     * @constructor
+     */
     function DBController() {}
 
+    /**
+     * Find repository in :user/:repo format from database
+     * 
+     * @method findRepo
+     * @param {String}   repoID UUID that can be accrued after calling DBController.addRepo function
+     * @param {Function} cb     Callback that will be called with DB data object where repository located in data.repository
+     */
     DBController.findRepo = function(repoID, cb) {
         db.user.getByID(repoID, cb);
     };
 
+    /**
+     * Add repository to DB
+     * 
+     * @method addRepo
+     * @param   {String} repository Repository to add in format :user/:repo
+     * @returns {String} Repo's UUID that can be used in DBController.findRepo function
+     */
     DBController.addRepo = function(repository) {
         if (repository) {
             var repoID = uuid.v1();
